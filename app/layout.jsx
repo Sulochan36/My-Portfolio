07c8147e6a2b015db06c6c73545c278295ccde9d
Sulochan Mahajan <sulochanmahajan@gmail.com>
2025-07-09 15:57:45 +0530

    Analytics added

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import Script from 'next/script';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
